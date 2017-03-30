a06e5b27fa00cf23b0890e4a30c70885caa95b26
jhong3842 <jhong3842@gmail.com>
2017-03-30 09:50:48 +0900

    Debuggi Attach, not yet Debug Handler

--- my_debugger.py
+++ my_debugger.py
@@ -1,6 +1,7 @@
 # -*- coding: cp949 -*-
 from ctypes import *
 from my_debugger_defines import *
+from _multiprocessing import *
 
 kernel32 = windll.kernel32
 
@@ -19,6 +20,7 @@ class debugger():
 
         #Struct to Object
         startupinfo = STARTUPINFO()
+        #프로세스 정보 
         process_information = PROCESS_INFORMATION()
 
         #프로세스가 독릭접으로 실행되게
@@ -44,4 +46,55 @@ class debugger():
         else:
             print "[*]Error : 0x08x." % kernel32.GetLastError()
 
+        #생성된 프로세스 핸들 저장
+        self.h_process = self.open_process(process_information.dwProcessId)
         
+    #Get the handle of pid
+    def open_process(self,pid):
+
+        h_process = kernel32.OpenProcess(win32.PROCESS_ALL_ACCESS, False, pid)
+        return h_process
+
+    def attach(self, pid):
+        self.h_process = self.open_process(pid)
+
+        #Change Controls of Debuggi to Debugger
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            sepf.pid            = int(pid)
+        else:
+            error = kernel32.GetLastError()
+            print "[*]Unable to attach to the process %d" % error
+
+    def run(self):
+        #Debugger run
+        while self.debugger_active == Ture:
+            self.get_debug_event()
+
+
+    def get_debug_event(slef):
+
+        #Debug Event Handler Pointer Structure
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+
+        #Not yet, Writing Code to handle Debug Event
+        
+        #Waiting occuring Debug Evnet
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            raw_input("Press a key to continue...")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent(\
+                debug_event.dwProcessId,\
+                debug_event.dwThreadId,\
+                continue_status)
+
+
+    def detach(self):
+
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print "[*] Finished debugging. Exiting..."
+            return True
+        else:
+            print "There was an error"
+            return False
--- my_test.py
+++ my_test.py
@@ -2,4 +2,12 @@ import my_debugger
 
 debugger = my_debugger.debugger()
 
-debugger.load("C:\\Windows\\System32\\calc.exe")
+#debugger.load("C:\\Windows\\System32\\calc.exe")
+
+pid = raw_input("Enter the PID of the process to attach to: ")
+
+
+#Attach Process 
+debugger.attach(int(pid))
+
+
